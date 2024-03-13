c41035946ffe050c43bfb6ee36eb631f081ec69b
Vishrut Jha <vishrut.jha@nighthack.in>
2024-03-14 00:58:49 +0530

    GoLive

--- js/main.js
+++ js/main.js
@@ -55,15 +55,15 @@ $(window).load(function(){
 /* END ------------------------------------------------------- */
 
 
-$('#countdown').countdown({
-	date: "Mar 14 2024",
-	render: function(data) {
-	  var el = $(this.el);
-	  el.empty()
-	    //.append("<div>" + this.leadingZeros(data.years, 4) + "<span>years</span></div>")
-	    .append("<div>" + this.leadingZeros(data.days, 2) + " <span>days</span></div>")
-	    .append("<div>" + this.leadingZeros(data.hours, 2) + " <span>hrs</span></div>")
-	    .append("<div>" + this.leadingZeros(data.min, 2) + " <span>min</span></div>")
-	    .append("<div>" + this.leadingZeros(data.sec, 2) + " <span>sec</span></div>");
-	}
-});
+// $('#countdown').countdown({
+// 	date: "Mar 14 2024",
+// 	render: function(data) {
+// 	  var el = $(this.el);
+// 	  el.empty()
+// 	    //.append("<div>" + this.leadingZeros(data.years, 4) + "<span>years</span></div>")
+// 	    .append("<div>" + this.leadingZeros(data.days, 2) + " <span>days</span></div>")
+// 	    .append("<div>" + this.leadingZeros(data.hours, 2) + " <span>hrs</span></div>")
+// 	    .append("<div>" + this.leadingZeros(data.min, 2) + " <span>min</span></div>")
+// 	    .append("<div>" + this.leadingZeros(data.sec, 2) + " <span>sec</span></div>");
+// 	}
+// });
